8697b08874ae4606962441d8839ea25ac82fa3de
SameerJadav <sameerjadav010@gmail.com>
2024-04-22 15:13:09 +0530

    add telescope fzf native

--- lua/sameer/plugins/telescope.lua
+++ lua/sameer/plugins/telescope.lua
@@ -1,16 +1,21 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup()
+		require("telescope").load_extension("fzf")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, {})
-		vim.keymap.set("n", "<leader>f", builtin.current_buffer_fuzzy_find, {})
-		vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>gs", builtin.grep_string, {})
+		vim.keymap.set("n", "<leader>df", builtin.current_buffer_fuzzy_find, {})
+		vim.keymap.set("n", "<leader>wf", builtin.live_grep, {})
 	end,
 }
