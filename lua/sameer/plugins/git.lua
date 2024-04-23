bdc252826c08a914c4116a56e8cd30704e137016
SameerJadav <sameerjadav010@gmail.com>
2024-04-23 11:22:46 +0530

    fix: i suck again

--- lua/sameer/plugins/git.lua
+++ lua/sameer/plugins/git.lua
@@ -1,6 +1,26 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			group = vim.api.nvim_create_augroup("SameerFugitive", { clear = true }),
+			pattern = "*",
+			callback = function()
+				if vim.bo.ft ~= "fugitive" then
+					return
+				end
+				local bufnr = vim.api.nvim_get_current_buf()
+				local map = function(keys, func)
+					vim.keymap.set("n", keys, func, { buffer = bufnr })
+				end
+
+				map("<leader>gc", function()
+					vim.cmd.Git("commit")
+				end)
+				map("<leader>gp", function()
+					vim.cmd.Git("push")
+				end)
+			end,
+		})
 	end,
 }
--- lua/sameer/plugins/lsp.lua
+++ lua/sameer/plugins/lsp.lua
@@ -102,12 +102,14 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
 			completion = { completeopt = "menu,menuone,noinsert" },
 			mapping = cmp.mapping.preset.insert({
 				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
