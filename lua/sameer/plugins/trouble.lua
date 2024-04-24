28b0b66f67def2d7f416854fad20a3cae9796b6a
SameerJadav <sameerjadav010@gmail.com>
2024-04-24 18:09:50 +0530

    add trouble

---
+++ lua/sameer/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		vim.keymap.set("n", "<leader>tt", function()
+			require("trouble").toggle()
+		end)
+	end,
+}
