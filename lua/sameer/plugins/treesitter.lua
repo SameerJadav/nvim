907d3b88ec7e6ac578067cda5afe3700fefd5c94
SameerJadav <sameerjadav010@gmail.com>
2024-05-17 18:07:43 +0530

    feat: add autotag plugin

--- lua/sameer/plugins/treesitter.lua
+++ lua/sameer/plugins/treesitter.lua
@@ -1,26 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {},
-	config = function()
-		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"go",
-				"gomod",
-				"gosum",
-				"javascript",
-				"typescript",
-				"bash",
-				"lua",
-				"luadoc",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {},
+    config = function()
+        require("nvim-treesitter.install").prefer_git = true
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "go",
+                "gomod",
+                "gosum",
+                "javascript",
+                "typescript",
+                "bash",
+                "lua",
+                "luadoc",
+                "vim",
+                "vimdoc",
+            },
+            autotag = {
+                enable = true,
+                enable_rename = true,
+                enable_close = true,
+                enable_close_on_slash = true,
+                filetypes = { "html", "markdown", "typescriptreact", "javascriptreact", "astro" },
+            },
+            auto_install = true,
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
