90d24f1cfaae321c29cb9dae1f13ee28a6c1f513
SameerJadav <sameerjadav010@gmail.com>
2024-05-20 18:15:33 +0530

    removed autotag plugin

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -4,7 +4,6 @@ return {
 		lazy = false,
 		priority = 100,
 		dependencies = {
-			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
@@ -15,11 +14,11 @@ return {
 			vim.opt.completeopt = { "menu", "menuone", "noselect" }
 			vim.opt.shortmess:append("c")
 
-			require("lspkind").init({})
-
 			local cmp = require("cmp")
+			local defaults = require("cmp.config.default")()
 
 			cmp.setup({
+				sorting = defaults.sorting,
 				sources = {
 					{ name = "nvim_lsp" },
 					{ name = "path" },
--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = { use_diagnostic_signs = true },
 	config = function()
 		vim.keymap.set("n", "<leader>tt", function()
 			require("trouble").toggle()
