210d22503f89454d7ec90deb599e56f355b05388
SameerJadav <sameerjadav010@gmail.com>
2024-06-09 18:50:49 +0530

    update core config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,40 +1,42 @@
 local set = vim.keymap.set
 
+-- General
 set("n", "<C-c>", "<cmd>nohlsearch<CR>")
-
-set("i", "<C-H>", "<C-W>")
-
 set("i", "<C-c>", "<Esc>")
 
+-- Editing
+set("i", "<C-H>", "<C-W>")
 set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 set("v", "J", ":m '>+1<cr>gv=gv")
 set("v", "K", ":m '<-2<cr>gv=gv")
+set("n", "J", "mzJ`z")
 
-set("n", "<leader>w", "<cmd>w<cr>", { silent = true })
-set("n", "<leader>q", "<cmd>q<cr>", { silent = true })
-set("n", "<leader>x", "<cmd>wq<cr>", { silent = true })
-
-set("n", "[d", vim.diagnostic.goto_prev)
-set("n", "]d", vim.diagnostic.goto_next)
-
+-- Navigation
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
-
 set("n", "n", "nzzzv")
 set("n", "N", "Nzzzv")
-
-set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/g<Left><Left><Left>]])
-set("n", "<leader>sc", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]])
-
-set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
 set("n", "<c-j>", "<c-w><c-j>")
 set("n", "<c-k>", "<c-w><c-k>")
 set("n", "<c-l>", "<c-w><c-l>")
 set("n", "<c-h>", "<c-w><c-h>")
 
+-- Diagnostics
+set("n", "[d", vim.diagnostic.goto_prev)
+set("n", "]d", vim.diagnostic.goto_next)
+
+-- Search and Replace
+set("n", "<leader>s", [[:%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>]])
+set("n", "<leader>sc", [[:%s/<C-r><C-w>/<C-r><C-w>/gc<Left><Left><Left>]])
+
+-- Buffers
+set("n", "<leader>w", "<cmd>w<cr>", { silent = true })
+set("n", "<leader>q", "<cmd>q<cr>", { silent = true })
+set("n", "<leader>x", "<cmd>wq<cr>", { silent = true })
+
+-- Terminal
+set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 set("n", "<leader>t", function()
 	vim.cmd.new()
 	vim.cmd.wincmd("J")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,40 +1,34 @@
+-- Global variables
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
 vim.g.have_nerd_font = true
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 
 local opt = vim.opt
 
-opt.clipboard = "unnamedplus"
-
+-- UI
 opt.number = true
 opt.relativenumber = true
-
 opt.wrap = false
-
 opt.hlsearch = true
 opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
+opt.signcolumn = "yes"
+opt.termguicolors = true
+opt.showmode = false
 
+-- Editing
 opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.breakindent = true
 
-opt.scrolloff = 10
-
-opt.signcolumn = "yes"
-
+-- Others
+opt.clipboard = "unnamedplus"
 opt.undofile = true
-
+opt.scrolloff = 10
 opt.inccommand = "split"
-
-opt.termguicolors = true
-
 opt.updatetime = 50
-
-opt.showmode = false
