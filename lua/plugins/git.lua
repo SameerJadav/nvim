520799c0022a16b73e0fa0afcf5d80029fa15fa2
SameerJadav <sameerjadav010@gmail.com>
2024-06-10 01:52:56 +0530

    open fugitive in new buffer

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,19 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>g", "<cmd>enew<cr><cmd>Git<cr><c-w><c-j><cmd>q<cr>")
 		vim.api.nvim_create_autocmd("BufWinEnter", {
 			group = vim.api.nvim_create_augroup("SameerFugitive", { clear = true }),
-			pattern = "*",
+			pattern = "fugitive://*",
 			callback = function()
-				if vim.bo.ft ~= "fugitive" then
-					return
-				end
 				local bufnr = vim.api.nvim_get_current_buf()
+
 				local map = function(keys, func)
 					vim.keymap.set("n", keys, func, { buffer = bufnr })
 				end
 
-				map("<leader>gc", function()
-					vim.cmd.Git("commit")
-				end)
-				map("<leader>gp", function()
-					vim.cmd.Git("push")
-				end)
+				map("<leader>gc", "<cmd>Git commit<cr>")
+				map("<leader>gp", "<cmd>Git push<cr>")
 			end,
 		})
 	end,
