1db86d74c6e90132c1890b6c84f4f4baf5acf775
SameerJadav <sameerjadav010@gmail.com>
2024-07-19 10:31:26 +0530

    change treesitter ensure installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,20 +9,9 @@ return {
 		require("nvim-treesitter.install").prefer_git = true
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = {
-				"go",
-				"gomod",
-				"gosum",
-				"javascript",
-				"typescript",
-				"bash",
-				"lua",
-				"luadoc",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = vim.fn.executable("tree-sitter") > 0,
+			ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "query" },
 			sync_install = false,
+			auto_install = vim.fn.executable("tree-sitter") > 0,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
